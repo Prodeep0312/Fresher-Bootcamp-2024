fd5f571c8634d94a6d421c817f16c4e99c4d94fe
Prodeep0312 <109973323+Prodeep0312@users.noreply.github.com>
2024-01-29 11:32:15 +0530

    Add files via upload

---
+++ Cpp Assessments/Scope.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int var = 0;
+namespace Special { int var = 100; }
+int main()
+{
+   int var = 10;
+   cout << setw(10) << var; // local block scope
+   {
+   	int var = 20;
+   	cout << setw(10) << var << endl; // local block scope
+   	{
+   		++var;
+   		cout << setw(10) << var; // local block scope
+   		cout << setw(10) << ++ ::var; // global scope
+   		cout << setw(10) << Special::var * 2 // Special namespace scope
+   			<< endl;
+   	}
+   	cout << setw(10) << var - ::var; // local block scope - global scope
+   }
+   cout << setw(10) << var << endl; // local block scope
+   return 0;
+}
+
+
+
+-------------------------------
+
+OUTPUT:
+
+10      20
+      	21      1     200
+	20      10	
+
+
+-----------------------------------
