e417c2a36b4a49c31d353e6ad9506e48646e61c0
Prodeep0312 <109973323+Prodeep0312@users.noreply.github.com>
2024-01-29 19:44:29 +0530

    Add files via upload

---
+++ Cpp Assessments/Macros/macros.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+#include "mathMacros.h"
+
+using namespace std;
+
+int main()
+
+{
+
+int num1=-7;
+
+int num2=9;
+
+cout<<"Absolute value of "<<num1<<":"<<ABS(num1)<<endl;
+
+cout<<"Absolute value of "<<num2<<":"<<ABS(num2)<<endl;
+
+cout<<"Max of "<<num1<<"and "<<num2<<":"<<MAX(num1,num2)<<endl;
+
+}
---
+++ Cpp Assessments/Macros/mathMacros.h
@@ -0,0 +1,10 @@
+
+
+#define ABS(x) ((x)<0 ?-(x):(x))
+
+#define MAX(a,b) ((a>b)? (a):(b))
+
+
+
+
+
